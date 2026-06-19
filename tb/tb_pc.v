dad8059b0c5da3d2243dd435bbb68dbefbc7bb09
KAVINA <kkavina361@gmail.com>
2026-06-19 22:28:43 +0530

    Update tb_pc.v

--- tb/tb_pc.v
+++ tb/tb_pc.v
@@ -1,26 +1,56 @@
 module tb_pc;
+
 reg clk;
 reg rst;
 reg ce;
 reg cl;
+reg load;
+reg [3:0] load_data;
+
 wire [3:0] pc_out;
-pc dut(.clk(clk),.rst(rst),.ce(ce),.cl(cl),.pc_out(pc_out));
-initial begin
+
+pc dut(
+    .clk(clk),
+    .rst(rst),
+    .ce(ce),
+    .cl(cl),
+    .load(load),
+    .load_data(load_data),
+    .pc_out(pc_out)
+);
+
+always #5 clk = ~clk;
+
+initial
+begin
     clk = 0;
-    forever #5 clk = ~clk;
-end
-initial begin
     rst = 1;
-    ce  = 0;
-    cl  = 0;
-    #20;
-    rst = 0;
+    ce = 0;
+    cl = 0;
+    load = 0;
+    load_data = 0;
+
+    #10 rst = 0;
+
     ce = 1;
-    #100;
+    #40;
+
+    ce = 0;
+    load = 1;
+    load_data = 4'd10;
+    #10;
+
+    load = 0;
+    ce = 1;
+    #20;
+
     cl = 1;
     #10;
+
     cl = 0;
-    #50;
-   $finish;
+    #20;
+
+    $finish;
 end
+
 endmodule
