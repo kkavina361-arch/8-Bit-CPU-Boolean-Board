18857d9f63203c308303bed39e497ca4d4fd1142
KAVINA <kkavina361@gmail.com>
2026-06-19 22:29:18 +0530

    Update ir.v

--- rtl/ir.v
+++ rtl/ir.v
@@ -1,19 +1,32 @@
-module ir( input clk,
-          input rst,
-          input ii,input [7:0] bus,
-          output reg [3:0] opcode,
-          output reg [3:0] operand);
+module ir(
+    input clk,
+    input rst,
+    input ii,
+
+    input [7:0] bus,
+
+    output reg [7:0] ir_data,
+    output reg [3:0] opcode,
+    output reg [3:0] operand
+);
+
 always @(posedge clk or posedge rst)
 begin
- if(rst)
- begin
- opcode  <= 4'd0;
- operand <= 4'd0;
- end
- else if(ii)
- begin
- opcode  <= bus[7:4];
- operand <= bus[3:0];
+
+    if(rst)
+    begin
+        ir_data <= 8'd0;
+        opcode  <= 4'd0;
+        operand <= 4'd0;
+    end
+
+    else if(ii)
+    begin
+        ir_data <= bus;
+        opcode  <= bus[7:4];
+        operand <= bus[3:0];
+    end
+
 end
-end
+
 endmodule
