aec053705f7f3001617a3cfef3a0ad672e23c7cd
KAVINA <kkavina361@gmail.com>
2026-06-19 20:33:56 +0530

    Update control_unit.v

--- rtl/control_unit.v
+++ rtl/control_unit.v
@@ -24,7 +24,6 @@ module control_unit(
     output reg [2:0] state_out
 );
 
-// Opcodes
 parameter NOP = 4'b0000;
 parameter IN  = 4'b0001;
 parameter STA = 4'b0010;
@@ -42,7 +41,6 @@ parameter DEC = 4'b1101;
 parameter CLR = 4'b1110;
 parameter SWP = 4'b1111;
 
-// Bus Select
 parameter BUS_NONE    = 3'b000;
 parameter BUS_PC      = 3'b001;
 parameter BUS_RAM     = 3'b010;
@@ -51,7 +49,6 @@ parameter BUS_ALU     = 3'b100;
 parameter BUS_OPERAND = 3'b101;
 parameter BUS_A       = 3'b110;
 
-// States
 parameter IDLE       = 3'd0;
 parameter FETCH1     = 3'd1;
 parameter FETCH2     = 3'd2;
@@ -59,12 +56,11 @@ parameter DECODE     = 3'd3;
 parameter EXEC1      = 3'd4;
 parameter EXEC2      = 3'd5;
 parameter WAIT_INPUT = 3'd6;
-parameter HALT       = 3'd7;
+parameter ST_HALT    = 3'd7;
 
 reg [2:0] state;
 reg [2:0] next_state;
 
-// State Register
 always @(posedge clk or posedge rst)
 begin
     if(rst)
@@ -73,7 +69,6 @@ begin
         state <= next_state;
 end
 
-// Next State Logic
 always @(*)
 begin
     next_state = state;
@@ -81,46 +76,26 @@ begin
     case(state)
 
         IDLE:
-        begin
             if(run)
                 next_state = FETCH1;
-            else
-                next_state = IDLE;
-        end
 
         FETCH1:
-        begin
             next_state = FETCH2;
-        end
 
         FETCH2:
-        begin
             next_state = DECODE;
-        end
 
         DECODE:
         begin
             case(opcode)
 
                 HLT:
-                    next_state = HALT;
+                    next_state = ST_HALT;
 
-                IN:
+                IN, INS:
                     next_state = WAIT_INPUT;
 
-                INS:
-                    next_state = WAIT_INPUT;
-
-                STA:
-                    next_state = EXEC1;
-
-                LDA:
-                    next_state = EXEC1;
-
-                LDB:
-                    next_state = EXEC1;
-
-                JMP:
+                STA, LDA, LDB, JMP:
                     next_state = EXEC1;
 
                 default:
@@ -130,55 +105,46 @@ begin
         end
 
         EXEC1:
-        begin
             next_state = EXEC2;
-        end
 
         EXEC2:
-        begin
             next_state = FETCH1;
-        end
 
         WAIT_INPUT:
         begin
             if(input_enter)
                 next_state = FETCH1;
-            else
-                next_state = WAIT_INPUT;
         end
 
-        HALT:
-        begin
-            next_state = HALT;
-        end
+        ST_HALT:
+            next_state = ST_HALT;
 
         default:
-        begin
             next_state = IDLE;
-        end
 
     endcase
 end
 
-// Output Control Logic
 always @(*)
 begin
-    pc_inc = 1'b0;
-    pc_load = 1'b0;
-    mar_load = 1'b0;
-    ir_load = 1'b0;
-    ram_we = 1'b0;
-    a_load = 1'b0;
-    b_load = 1'b0;
-    out_load = 1'b0;
-    flag_load = 1'b0;
-    swap_en = 1'b0;
+
+    pc_inc = 0;
+    pc_load = 0;
+    mar_load = 0;
+    ir_load = 0;
+    ram_we = 0;
+    a_load = 0;
+    b_load = 0;
+    out_load = 0;
+    flag_load = 0;
+    swap_en = 0;
 
     alu_op = opcode;
     bus_sel = BUS_NONE;
 
-    input_wait_led = 1'b0;
-    halt_led = 1'b0;
+    input_wait_led = 0;
+    halt_led = 0;
+
     state_out = state;
 
     case(state)
@@ -186,24 +152,24 @@ begin
         FETCH1:
         begin
             bus_sel = BUS_PC;
-            mar_load = 1'b1;
+            mar_load = 1;
         end
 
         FETCH2:
         begin
             bus_sel = BUS_RAM;
-            ir_load = 1'b1;
-            pc_inc = 1'b1;
+            ir_load = 1;
+            pc_inc = 1;
         end
 
         WAIT_INPUT:
         begin
-            input_wait_led = 1'b1;
+            input_wait_led = 1;
 
             if(input_enter)
             begin
                 bus_sel = BUS_INPUT;
-                a_load = 1'b1;
+                a_load = 1;
             end
         end
 
@@ -211,33 +177,16 @@ begin
         begin
             case(opcode)
 
-                STA:
+                STA, LDA, LDB:
                 begin
                     bus_sel = BUS_OPERAND;
-                    mar_load = 1'b1;
-                end
-
-                LDA:
-                begin
-                    bus_sel = BUS_OPERAND;
-                    mar_load = 1'b1;
-                end
-
-                LDB:
-                begin
-                    bus_sel = BUS_OPERAND;
-                    mar_load = 1'b1;
+                    mar_load = 1;
                 end
 
                 JMP:
                 begin
                     bus_sel = BUS_OPERAND;
-                    pc_load = 1'b1;
-                end
-
-                default:
-                begin
-                    bus_sel = BUS_NONE;
+                    pc_load = 1;
                 end
 
             endcase
@@ -250,90 +199,45 @@ begin
                 STA:
                 begin
                     bus_sel = BUS_A;
-                    ram_we = 1'b1;
+                    ram_we = 1;
                 end
 
                 LDA:
                 begin
                     bus_sel = BUS_RAM;
-                    a_load = 1'b1;
+                    a_load = 1;
                 end
 
                 LDB:
                 begin
                     bus_sel = BUS_RAM;
-                    b_load = 1'b1;
+                    b_load = 1;
                 end
 
-                ADD:
+                ADD, SUB, MUL, INC, DEC, CLR:
                 begin
                     bus_sel = BUS_ALU;
-                    a_load = 1'b1;
-                    flag_load = 1'b1;
-                end
-
-                SUB:
-                begin
-                    bus_sel = BUS_ALU;
-                    a_load = 1'b1;
-                    flag_load = 1'b1;
-                end
-
-                MUL:
-                begin
-                    bus_sel = BUS_ALU;
-                    a_load = 1'b1;
-                    flag_load = 1'b1;
-                end
-
-                INC:
-                begin
-                    bus_sel = BUS_ALU;
-                    a_load = 1'b1;
-                    flag_load = 1'b1;
-                end
-
-                DEC:
-                begin
-                    bus_sel = BUS_ALU;
-                    a_load = 1'b1;
-                    flag_load = 1'b1;
-                end
-
-                CLR:
-                begin
-                    bus_sel = BUS_ALU;
-                    a_load = 1'b1;
-                    flag_load = 1'b1;
+                    a_load = 1;
+                    flag_load = 1;
                 end
 
                 OUT:
                 begin
                     bus_sel = BUS_A;
-                    out_load = 1'b1;
+                    out_load = 1;
                 end
 
                 SWP:
                 begin
-                    swap_en = 1'b1;
-                end
-
-                default:
-                begin
-                    bus_sel = BUS_NONE;
+                    swap_en = 1;
                 end
 
             endcase
         end
 
-        HALT:
+        ST_HALT:
         begin
-            halt_led = 1'b1;
-        end
-
-        default:
-        begin
-            bus_sel = BUS_NONE;
+            halt_led = 1;
         end
 
     endcase
